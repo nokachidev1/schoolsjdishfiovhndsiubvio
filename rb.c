e0f314f0af797146a2ed86ce1dfacb7f97da6534
Pheerapat Rinnasak <151990693+nokachidev1@users.noreply.github.com>
2026-06-26 15:47:00 +0700

    Correct point calculation and condition logic
    Fix logic errors in point calculation and check condition.

---
+++ rb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+  int product_one_price = 450;
+  int product_two_price = 320;
+  int product_three_price = 380;
+  int subtotal = product_one_price + product_two_price + product_three_price;
+  int point_gained = subtotal / 50;
+  int check = subtotal > 1000
+  
+  if check = 1;
+    point = point_gained / 100 * 20;
+  else;
+    point = point_gained / 100 * 20;
+
+  printf("Total = %d\n", subtotal);
+  printf("Point = %d\n", point_gained);
+  printf("Bonus Point = %d\n", point);
+  printf("Total Point = %d\n", point_gained + point);
+  printf("Over1000 = %d\n", check);
+}
+  
