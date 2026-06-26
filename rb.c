f79a6802a5c1a76d41b479927aaa3b495189ccc5
Pheerapat Rinnasak <151990693+nokachidev1@users.noreply.github.com>
2026-06-26 16:10:13 +0700

    Fix rb.c: syntax errors, declare point, correct bonus calculation, add return 0

--- rb.c
+++ rb.c
@@ -1,22 +1,26 @@
 #include <stdio.h>
 
-int main() {
+int main(void) {
   int product_one_price = 450;
   int product_two_price = 320;
   int product_three_price = 380;
   int subtotal = product_one_price + product_two_price + product_three_price;
   int point_gained = subtotal / 50;
-  int check = subtotal > 1000
-  
-  if check = 1;
-    point = point_gained / 100 * 20;
-  else;
-    point = point_gained / 100 * 20;
+  int check = subtotal > 1000;
+  int point;
+
+  if (check == 1) {
+    /* apply 20% bonus to points when subtotal > 1000 */
+    point = (point_gained * 20) / 100;
+  } else {
+    point = 0;
+  }
 
   printf("Total = %d\n", subtotal);
   printf("Point = %d\n", point_gained);
   printf("Bonus Point = %d\n", point);
   printf("Total Point = %d\n", point_gained + point);
   printf("Over1000 = %d\n", check);
+
+  return 0;
 }
-  
